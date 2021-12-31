ffcc7cedabd9ee444848b49ef3cd6630cb2fa89d
unknown <sagie.choi@gmail.com>
2021-12-31 23:56:14 +0900

    landing page 제작

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '58680b4a7b798b4e3627508f75a339f6';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
